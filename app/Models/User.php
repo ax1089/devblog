e2fe549688f193961bd51fa16a9f58556ec5928b
wencai159@126.com <wencai159@126.com>
2022-02-13 17:20:53 +0800

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Notifications\Notifiable;
+//use Laravel\Sanctum\HasApiTokens;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+//    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, MustVerifyEmailTrait;
     /**
      * The attributes that are mass assignable.
      *
